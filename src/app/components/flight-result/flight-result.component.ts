698e0e4faa852df99ebec544489c8ac26db45469
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-11-02 14:19:01 +0200

    make a loader display

--- src/app/components/flight-result/flight-result.component.ts
+++ src/app/components/flight-result/flight-result.component.ts
@@ -1,6 +1,6 @@
 import { AirItinerary } from 'src/app/model/flight.model';
 import { flightService } from './../../services/flight.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, filter, tap } from 'rxjs';
 
 @Component({
@@ -8,9 +8,16 @@ import { Observable, filter, tap } from 'rxjs';
   templateUrl: './flight-result.component.html',
   styleUrls: ['./flight-result.component.scss'],
 })
-export class FlightResultComponent {
+export class FlightResultComponent implements OnInit {
   constructor(private flightService: flightService) {}
   displayFilters: boolean = false;
   flightDetails$: Observable<AirItinerary[]> = this.flightService.flights$;
   FlightAirline$: Observable<string[]> = this.flightService.airLines$;
+  loading = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 3000);
+  }
 }
