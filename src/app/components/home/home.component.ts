c75a1b37d54ec64f826e75811d305116c3be7f1a
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-11-02 16:17:01 +0200

    add typed.js to the home screen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  @ViewChild('describe', { static: true }) describe!: ElementRef;
 
+  ngOnInit(): void {
+    const typed = new Typed(this.describe.nativeElement, {
+      strings: [
+        'Explore breathtaking destinations.',
+        'Book your dream getaway today!',
+        'Discover the world with exclusive deals.',
+        'Adventure awaits around every corner.',
+      ],
+      typeSpeed: 50,
+      backSpeed: 30,
+      backDelay: 1000,
+      loop: true,
+      showCursor: false,
+    });
+  }
 }
